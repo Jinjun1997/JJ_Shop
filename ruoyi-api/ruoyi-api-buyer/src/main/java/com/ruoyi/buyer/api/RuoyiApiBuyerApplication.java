42d33527f2cc4f57c17752d6347f16bd14536c72
hejinjun <1332300886@qq.com>
2022-12-27 06:31:57 +0800

    新增目录

---
+++ ruoyi-api/ruoyi-api-buyer/src/main/java/com/ruoyi/buyer/api/RuoyiApiBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.ruoyi.buyer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiApiBuyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiApiBuyerApplication.class, args);
+    }
+
+}
